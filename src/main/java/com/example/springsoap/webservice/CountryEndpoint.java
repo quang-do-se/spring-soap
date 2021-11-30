fd74aaee4880ca604deca81bb742158b759a1b6f
Quang Do <quang.do@colorado.edu>
2021-11-30 16:59:21 -0700

    Update

---
+++ src/main/java/com/example/springsoap/webservice/CountryEndpoint.java
@@ -0,0 +1,32 @@
+package com.example.producingwebservice;
+
+import com.example.springsoap.webservice.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import io.spring.guides.spring_soap.GetCountryRequest;
+import io.spring.guides.spring_soap.GetCountryResponse;
+
+@Endpoint
+public class CountryEndpoint {
+	private static final String NAMESPACE_URI = "http://spring.io/guides/spring-soap";
+
+	private CountryRepository countryRepository;
+
+	@Autowired
+	public CountryEndpoint(CountryRepository countryRepository) {
+		this.countryRepository = countryRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+	@ResponsePayload
+	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+		GetCountryResponse response = new GetCountryResponse();
+		response.setCountry(countryRepository.findCountry(request.getName()));
+
+		return response;
+	}
+}
