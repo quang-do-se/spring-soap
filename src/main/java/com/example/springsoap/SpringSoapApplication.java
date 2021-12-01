1652063b1d0e49423ed07e06d39993421764df9e
Quang Do <quang.do@colorado.edu>
2021-12-01 16:34:47 -0700

    Clean up

--- src/main/java/com/example/springsoap/SpringSoapApplication.java
+++ src/main/java/com/example/springsoap/SpringSoapApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringSoapApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringSoapApplication.class, args);
     }
-
 }
