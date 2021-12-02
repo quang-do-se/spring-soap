c30c49109a60f98e3b3d29167c7e0a62f4914b37
Quang Do <quang.do@colorado.edu>
2021-12-02 13:50:26 -0700

    Refactor

--- src/main/java/com/example/springsoap/webservice/EnrollmentWebServiceEndpoint.java
+++ src/main/java/com/example/springsoap/webservice/EnrollmentWebServiceEndpoint.java
@@ -1,7 +1,11 @@
 package com.example.springsoap.webservice;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.colorado.oit.es.enrollments.ConstituentEnrollmentData;
 import edu.colorado.oit.es.enrollments.EnrollmentResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
@@ -11,25 +15,40 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 @Endpoint
 public class EnrollmentWebServiceEndpoint {
-	private static final String NAMESPACE_URI = "http://www.cu.edu/integration/ConstituentEnrlData";
+    private static final Logger logger = LoggerFactory.getLogger(EnrollmentWebServiceEndpoint.class);
+    private static final String NAMESPACE_URI = "http://www.cu.edu/integration/ConstituentEnrlData";
+    public static final String JMS_ENROLLMENTS_QUEUE = "jmsIncomingEnrollmentsQueue";
 
-	public static final String JMS_ENROLLMENTS_QUEUE = "jmsIncomingEnrollmentsQueue";
+    private final JmsTemplate jmsTemplate;
 
-	private final JmsTemplate jmsTemplate;
+    @Autowired
+    public EnrollmentWebServiceEndpoint(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
+    }
 
-	@Autowired
-	public EnrollmentWebServiceEndpoint(JmsTemplate jmsTemplate) {
-		this.jmsTemplate = jmsTemplate;
-	}
+    @PayloadRoot(localPart = "ConstituentEnrollmentData", namespace = NAMESPACE_URI)
+    @ResponsePayload
+    public EnrollmentResponse consumeEnrollment(@RequestPayload ConstituentEnrollmentData enrollmentData) {
+        logReceivedEnrollment(enrollmentData);
 
-	@PayloadRoot(localPart = "ConstituentEnrollmentData", namespace = NAMESPACE_URI)
-	@ResponsePayload
-	public EnrollmentResponse consumeEnrollment(@RequestPayload ConstituentEnrollmentData enrollmentData) {
-		// Send message to queue
-		jmsTemplate.convertAndSend(JMS_ENROLLMENTS_QUEUE, enrollmentData);
+        jmsTemplate.convertAndSend(JMS_ENROLLMENTS_QUEUE, enrollmentData);
+        logger.info("Sent enrollment to queue.");
 
-		EnrollmentResponse response = new EnrollmentResponse();
-		response.setMessage("SUCCESS");
-		return response;
-	}
+        return buildResponse();
+    }
+
+    private EnrollmentResponse buildResponse() {
+        EnrollmentResponse response = new EnrollmentResponse();
+        response.setMessage("SUCCESS");
+        return response;
+    }
+
+    private void logReceivedEnrollment(ConstituentEnrollmentData enrollmentData) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            logger.info("Enrollment received from SOAP: " + mapper.writeValueAsString(enrollmentData));
+        } catch (JsonProcessingException e) {
+            logger.error(e.getMessage());
+        }
+    }
 }
